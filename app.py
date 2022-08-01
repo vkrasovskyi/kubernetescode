9a00f1afade8fc7f81ed59fbdc5aaa1c0964f6a6
vkrasovskyi <vasyl.krasovskiy@true-item.com>
2022-08-01 16:45:53 +0300

    update

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY Test25!!'
+    return 'Please subscribe, like, and comment on this video, TY Test25 Set new!!'
